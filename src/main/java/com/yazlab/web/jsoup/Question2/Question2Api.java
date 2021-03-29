49c59d5f421aab9e9fdf77d174728c44dadd2878
AYasarKisa <60048336+AYasarKisa@users.noreply.github.com>
2021-03-29 16:20:48 +0300

    Question4 Add Sort

--- src/main/java/com/yazlab/web/jsoup/Question2/Question2Api.java
+++ src/main/java/com/yazlab/web/jsoup/Question2/Question2Api.java
@@ -12,7 +12,7 @@ public class Question2Api {
     @GetMapping("/keywords")
     public List<Keywords> get(@RequestParam("url") String url){
         System.out.println("Question2 Api Run\n\n\n");
-
+        
         Question2 question2 = new Question2();
         return question2.extractKeywords(url);
     }
